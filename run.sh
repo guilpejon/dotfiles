e109e3427a962e3d66321f644ccb25c2b0775d33
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2022-02-18 22:49:17 -0300

    Added asdf installation

--- run.sh
+++ run.sh
@@ -42,5 +42,14 @@ brew install telegram
 brew install whatsapp
 brew install 1password
 
+# Install asdf
+brew install asdf
+echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
+asdf plugin add nodejs ruby # asdf list
+# asdf list-all <PLUGIN>
+# asdf install <PLUGIN> <VERSION> 
+# global config: asdf global nodejs <VERSION>
+# local config: asdf local nodejs <VERSION>
+
 # Install dev tools
 
