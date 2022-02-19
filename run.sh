c8b52758da62ba293bfaaa2d57743546ab8b850d
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2022-02-18 23:05:10 -0300

    Added dev tools and git config

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-# Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/guilpejon/.zprofile
-
 # Install Rosetta 2
 /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
 # Intall xcode
 xcode-select --install
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/guilpejon/.zprofile
 
 # Install iterm2
 brew install iterm2
@@ -16,7 +16,6 @@ brew install neovim
 brew install yarn
 brew install npm
 curl https://sh.rustup.rs -sSf | sh
-
 # Install lunarvim
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 echo "export PATH=$HOME/.local/bin:$HOME/.cargo/bin:$PATH" >> ~/.zshrc
@@ -31,9 +30,6 @@ echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 brew install romkatv/powerlevel10k/powerlevel10k
 echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
 
-# Install rust apps
-cargo install bat exa tokei ytop tealdeer grex zoxide git-delta
-
 # Install common apps
 brew install slack
 brew install spotify
@@ -45,11 +41,30 @@ brew install 1password
 # Install asdf
 brew install asdf
 echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
-asdf plugin add nodejs ruby # asdf list
+asdf plugin add nodejs 
+asdf plugin add ruby
+# asdf list
 # asdf list-all <PLUGIN>
 # asdf install <PLUGIN> <VERSION> 
 # global config: asdf global nodejs <VERSION>
 # local config: asdf local nodejs <VERSION>
 
 # Install dev tools
+cargo install bat exa tokei ytop tealdeer grex zoxide git-delta
+brew install diff-so-fancy
+brew install watch
+brew install wget
+brew install wireguard-tools
+brew install gh
+brew install cask
+brew install --cask ngrok
+brew tap heroku/brew
+brew install heroku
+heroku plugins:install heroku-accounts
 
+# Git config
+git config --global user.name "NAME"
+git config --global user.email "EMAIL"
+git config --global core.editor "lvim"
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
