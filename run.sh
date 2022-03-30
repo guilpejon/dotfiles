371f3fe19385a70bb19fe5fc8207a00f6e2324e2
Guilherme Pejon <guilpejon@gmail.com>
2022-03-30 10:07:28 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -38,17 +38,6 @@ brew install telegram
 brew install whatsapp
 brew install 1password
 
-# Install asdf
-brew install asdf
-echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
-asdf plugin add nodejs 
-asdf plugin add ruby
-# asdf list
-# asdf list-all <PLUGIN>
-# asdf install <PLUGIN> <VERSION> 
-# global config: asdf global nodejs <VERSION>
-# local config: asdf local nodejs <VERSION>
-
 # Install dev tools
 cargo install bat exa tokei ytop tealdeer grex zoxide git-delta
 brew install watch
