eb96e80aa532560269c85c9181fc90dcd9b58570
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2022-10-01 15:33:14 -0300

    Improved aliases

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -20,13 +20,7 @@ keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
-keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sl', '<C-w>l')
-
--- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+keymap.set('n', '<S-LEFT>', '<C-w>h')
+keymap.set('n', '<S-UP>', '<C-w>k')
+keymap.set('n', '<S-DOWN>', '<C-w>j')
+keymap.set('n', '<S-RIGHT>', '<C-w>l')
