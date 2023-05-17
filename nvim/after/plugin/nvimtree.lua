70d9e1dc85a03c2f2a845ca5048874a9a27dce72
Guilherme Pejon <guilpejon@gmail.com>
2023-05-17 20:21:45 -0300

    Cleaned up nvimtree config

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -1,12 +1,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- always open the tree
-local function open_nvim_tree()
-  require("nvim-tree.api").tree.open()
-end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
@@ -14,17 +8,13 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
   },
   renderer = {
     group_empty = true,
   },
   filters = {
     dotfiles = false,
+
   },
 })
 
