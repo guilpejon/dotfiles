f12b46ef5aff228be482999ff4a2f9e39b3dda66
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2023-11-30 23:45:47 -0300

    Added github copilot

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
