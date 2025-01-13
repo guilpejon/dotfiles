ea927f354f0ff6574b3530ec756580d91cdfd261
Guilherme Pejon <guilherme.pejon@housecallpro.com>
2025-01-13 17:17:41 -0300

    Updated gen configs

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -10,9 +10,9 @@ return {
     accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
     host = "localhost", -- The host running the Ollama service.
     port = "11434", -- The port on which the Ollama service is listening.
-    display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
+    display_mode = "split", -- The display mode. Can be "float" or "split" or "horizontal-split".
     show_prompt = false, -- Shows the prompt submitted to Ollama.
-    show_model = false, -- Displays which model you are using at the beginning of your chat session.
+    show_model = true, -- Displays which model you are using at the beginning of your chat session.
     no_auto_close = false, -- Never closes the window automatically.
     file = false, -- Write the payload to a temporary file to keep the command short.
     hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
